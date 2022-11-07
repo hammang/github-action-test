7d07d92ab0b18b4f4605d460885046e4a114b2e1
Jinho Kim <tiger.ohgirafferes@gmail.com>
2022-11-07 15:50:15 +0900

    오타 수정

--- src/main/java/com/ohgiraffers/githubactiontest/ResponseMessage.java
+++ src/main/java/com/ohgiraffers/githubactiontest/ResponseMessage.java
@@ -3,18 +3,18 @@ package com.ohgiraffers.githubactiontest;
 public class ResponseMessage {
 
     private String text;
-    private String desciption;
+    private String description;
 
     public ResponseMessage(String text, String description) {
         this.text = text;
-        this.desciption = description;
+        this.description = description;
     }
 
     public String getText() {
         return text;
     }
 
-    public String getDesciption() {
-        return desciption;
+    public String getDescription() {
+        return description;
     }
 }
